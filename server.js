2780fc8c3de3f069be13db827f9c674d2af17e02
conconartist <conconart.info@gmail.com>
2022-01-07 18:36:40 -0700

    Add get points

--- server.js
+++ server.js
@@ -4,15 +4,32 @@ const app = express();
 const userData = require('./userData.js');
 
 app.locals.users = userData;
+app.locals.sleeps = sleepData;
+app.locals.hydrations = hydrationData;
+app.locals.activities = activityData;
 
 app.use(express.json());
 app.use(cors());
 
 app.set('port', 3001)
-app.listen(app.get('port'), () => {
-    console.log(`Now listening on port ${app.get('port')}!`)
-})
 
 app.get('/api/v1/users', (request, response) => {
     response.status(200).json(app.locals.users)
 })
+
+app.get('/api/v1/sleeps', (request, response) => {
+    response.status(200).json(app.locals.sleeps)
+})
+
+app.get('/api/v1/hydrations', (request, response) => {
+    response.status(200).json(app.locals.hydrations)
+})
+
+app.get('/api/v1/activities', (request, response) => {
+    response.status(200).json(app.locals.activities)
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Now listening on port ${app.get('port')}!`)
+})
+
